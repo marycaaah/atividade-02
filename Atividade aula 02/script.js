91199234d66f4aa481e074ecced78e1dc4895007
marycaaah <106408633+marycaaah@users.noreply.github.com>
2022-10-26 17:16:48 -0300

    aula 02

---
+++ Atividade aula 02/script.js
@@ -0,0 +1,19 @@
+/*Crie o seguinte array: var deuses = ['Aegir', 'Aud', 'Balder']
+Adicione os 3 Deuses: ('Loki', 'Odin', 'Frey') com a função push() */
+
+var Deuses= ['Aegir', 'Aud', 'Balder'];
+Deuses.push ('Loki','Odin', 'Frey');
+console.log(Deuses);
+
+/*Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma crescente dica: .sort */
+
+let number= [5,7,1,8,9];
+number.sort();
+console.log(number);
+
+/*Crie o seguinte array: var numbers = [5, 7, 1, 8, 9]
+E exiba de forma decrescente.*/
+ let numbers = [5,7,1,8,9]
+ numbers.reverse()
+ console.log(numbers);
